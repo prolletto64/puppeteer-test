78110bd9f394eda51bcdcc8b0864d79dcc06a433
prolletto64 <poiluonzo@mail.com>
2023-09-21 19:35:00 +0200

    will scrape ebay

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { randomUUID } from "crypto";
 import puppeteer from "puppeteer";
 import fs from "fs";
+import path from "path";
 
-async function run() {
-  if (!fs.existsSync("images")) fs.mkdirSync("images");
-  const browser = await puppeteer.launch({ headless: false });
-  for (let i = 0; i < 100; i++) {
-    console.log(i);
-    let page = await browser.newPage();
-    await page.goto("https://presearch.com/");
-    await page.screenshot({ path: `images/${randomUUID()}.png` });
-    page.close();
-  }
-  browser.close();
+async function getCategory(c?: string) {
+  if (!c)
+    c =
+      "https://www.ebay.it/b/Computer-portatili-laptop-e-notebook/175672/bn_16546646";
+  if (!fs.existsSync("cache")) fs.mkdirSync("cache");
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: path.join(process.cwd(), "cache"),
+  });
+
+  let page = (await browser.pages())[0];
+  await page.goto(c, { waitUntil: "domcontentloaded" });
 }
 
-run();
+getCategory();
