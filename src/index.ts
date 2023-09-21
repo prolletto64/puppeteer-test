f44b933ad23a1ec4ad86eb93e694691faf58471d
prolletto64 <poiluonzo@mail.com>
2023-09-21 22:10:00 +0200

    got lots of laptops

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,26 @@ async function getCategory(category?: string, filtered?: boolean) {
 
   let page = (await browser.pages())[0];
   await page.goto(category, { waitUntil: "domcontentloaded" });
+  const productsEl = await page.$$(".s-item__wrapper.clearfix");
+  let products = [];
+  /*   let should = true;
+  let i = 1;
+  while (should) {
+    console.log(i); */
+  await Promise.all(
+    productsEl.map(async (p) => {
+      let name = await p.$eval(".s-item__title ", (el) => el.innerText.trim());
+      let price = await p.$eval(".s-item__price", (el) => el.innerText.trim());
+      let image = await p.$eval(".s-item__image-img", (el) =>
+        el.getAttribute("src")
+      );
+      products.push({ name, price, image });
+    })
+  );
+  /* i++;
+    should = (await page.$(".pagination__next")) != null;
+    if (should) (await page.$(".pagination__next"))!.click();
+  } */
 }
 
 getCategory(undefined, true);
