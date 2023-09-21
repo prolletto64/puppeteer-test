e0a35ee57483a9beaecf589bb3fb20514222770b
prolletto64 <poiluonzo@mail.com>
2023-09-21 18:39:28 +0200

    first working code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("it works");
+import { randomUUID } from "crypto";
+import puppeteer from "puppeteer";
+import fs from "fs";
+
+async function run() {
+  if (!fs.existsSync("images")) fs.mkdirSync("images");
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto("https://presearch.com/");
+  await page.screenshot({ path: `images/${randomUUID()}.png` });
+  browser.close();
+}
+
+run();
