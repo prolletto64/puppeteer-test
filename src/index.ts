da72a67aca208efb66d62f1325ec4c3a2af66d74
prolletto64 <poiluonzo@mail.com>
2023-09-21 19:51:32 +0200

    added filtering

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 import path from "path";
 
-async function getCategory(c?: string) {
-  if (!c)
-    c =
+async function getCategory(category?: string, filtered?: boolean) {
+  if (!category)
+    category =
       "https://www.ebay.it/b/Computer-portatili-laptop-e-notebook/175672/bn_16546646";
   if (!fs.existsSync("cache")) fs.mkdirSync("cache");
+  if (filtered) category += "?rt=nc&LH_ItemCondition=1000&mag=1";
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -14,7 +15,7 @@ async function getCategory(c?: string) {
   });
 
   let page = (await browser.pages())[0];
-  await page.goto(c, { waitUntil: "domcontentloaded" });
+  await page.goto(category, { waitUntil: "domcontentloaded" });
 }
 
-getCategory();
+getCategory(undefined, true);
